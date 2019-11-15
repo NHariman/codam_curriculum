903c8e4b86fe1cf548d1e7891e09236c4a0c252a
Naomi Hariman <nhariman@f1r5s3.codam.nl>
2019-11-15 19:01:58 +0100

    added header file

--- libft_bonus.h
+++ libft_bonus.h
@@ -6,12 +6,14 @@
 /*   By: nhariman <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/14 19:17:27 by nhariman       #+#    #+#                */
-/*   Updated: 2019/11/14 22:52:59 by nhariman      ########   odam.nl         */
+/*   Updated: 2019/11/15 18:48:25 by nhariman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_BONUS_H
 # define LIBFT_BONUS_H
+
+# include <stdlib.h>
 
 typedef struct		s_list
 {
@@ -23,6 +25,7 @@ void				ft_lstadd_front(t_list **alst, t_list *new);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **alst, t_list *new);
-void				ft_lstclear(t_list **lst, void (*del)(void *));
+//void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
 #endif
